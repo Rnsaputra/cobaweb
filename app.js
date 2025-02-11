d934a61027da025fd78b0868e54cb7a72be21efc
Rnsaputra <rnsaputra12@gmail.com>
2025-02-11 10:23:36 +0700

    cobaweb

---
+++ app.js
@@ -0,0 +1,123 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper(".mySwiper", {
+        grabcursor: true,
+        effect: "coverflow",
+        centeredSlides: true,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 3,
+            slideShadow: true
+        },
+        autoplay: {
+            delay: 5000,
+        },
+        slidesPerView: 2, // 1 card terlihat di layar kecil
+        spaceBetween: 20, // Jarak antar card
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2
+            }, 
+            1024: {
+                slidesPerView: 4,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 3,
+                    slideShadow: true
+                }
+            } 
+        },
+
+
+    });
+    new Swiper(".mySwiper2", {
+        grabcursor: true,
+        effect: "coverflow",
+        centeredSlides: true,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 3,
+            slideShadow: true
+        },
+        autoplay: {
+            delay: 4500,
+        },
+        slidesPerView: 2, // 1 card terlihat di layar kecil
+        spaceBetween: 20, // Jarak antar card
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2
+            }, // 2 card terlihat di layar >= 640px
+            1024: {
+                slidesPerView: 4,
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 3,
+                    slideShadow: true
+                }
+            } // 3 card terlihat di layar >= 1024px
+        },
+
+
+    });
+    new Swiper(".mySwiper3", {
+        grabcursor: true,
+        
+        loop: true,
+        centeredSlides: true,
+        flipEffect: {
+            rotate: 0,
+            stretch: 80,
+            depth: 350,
+            modifier: 1,
+            slideShadow: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+        slidesPerView: 2, // 1 card terlihat di layar kecil
+        spaceBetween: 20, // Jarak antar card
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2
+            }, // 2 card terlihat di layar >= 640px
+            1024: {
+                slidesPerView: 4,
+
+            } // 3 card terlihat di layar >= 1024px
+        },
+
+
+    });
+});
